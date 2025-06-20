f501156c0f396c35deb505aa2928849b564fd430
bhupendrayz <132221281+bhupendrayz@users.noreply.github.com>
2025-06-20 17:28:26 +0530

    dfsdesdfe

--- second.py
+++ second.py
@@ -1,9 +1,5 @@
-print("Hello, World!")
-print('abc')
-print("This is a test.")
+print("Hello, World Akansha")
 print('This is another test.')
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import requests
